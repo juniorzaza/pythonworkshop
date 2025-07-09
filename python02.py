80d81ca28d448dd3a7d01643b597ff2561cf433a
juniorzaza <chnolasit2546@gmil.com>
2025-07-09 18:39:49 +0700

    v1 python01+07

---
+++ python02.py
@@ -0,0 +1,27 @@
+# ตัวแปร variable
+fullname = "ชนินทร"  #ข้อมูล string
+age = 25  #ข้อมูล number
+major = 'IoT' #ข้อมูล string
+stuStatus = True  #หรือ False เรียกว่าเป็นข้อมูล boolean
+detail = '''
+            หล่อ
+            รวย
+            เก่ง
+            เท่ห์
+         ''' #ข้อมูล string
+foodLove = """
+            KFC
+                 Pizza
+  Expresso
+           """ #ข้อมูล string
+ 
+total = 10 + 30 + 40  #expression นิพจน์ คือ 10 + 30 + 40 คือ อะไรที่มีโพรเซสการทำงาน
+ 
+print(fullname)
+print(age)
+print(major)
+print(stuStatus)
+print(detail)
+print(foodLove)
+ 
+ 
